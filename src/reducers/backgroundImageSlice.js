731256469e009d98b8d44c003d356426e9a4e1b2
Name <st.grigelevich@gmail.com>
2022-12-28 23:04:24 +0000

    Fixed jittering in MainPage

--- src/reducers/backgroundImageSlice.js
+++ src/reducers/backgroundImageSlice.js
@@ -1,4 +1,4 @@
-import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchBackgroundImage } from "../api/fetchApi";
 import choose7RandomCategories from "../components/mostPopularCategories";
 import { getBackgroundImage } from "../filters/filters";
@@ -28,66 +28,7 @@ const backgroundImageSlice = createSlice({
     },
     isFetched: false,
     suggestions: choose7RandomCategories()
-  },
-  // initialState: {
-  //   image: {
-  //     id: 2775196,
-  //     width: 5444,
-  //     height: 3629,
-
-  //     alt: "White Clouds",
-
-  //     avg_color: "#7A6A83",
-
-  //     height: 3629,
-
-  //     id: 2775196,
-
-  //     liked: false,
-
-  //     photographer: "Roberto Nickson",
-
-  //     photographer_id: 1268114,
-
-  //     photographer_url: "https://www.pexels.com/@rpnickson",
-
-  //     src: {
-  //       original:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg",
-  //       large2x:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  //       large:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
-
-  //       landscape:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
-
-  //       large:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
-
-  //       large2x:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-
-  //       medium:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&h=350",
-
-  //       original:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg",
-
-  //       portrait:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
-
-  //       small:
-  //         "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&h=130",
-
-  //       tiny: "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280",
-
-  //       url: "https://www.pexels.com/photo/white-clouds-2775196/",
-
-  //       width: 5444,
-  //     },
-  //   },
-  // },
+  },  
   reducers: {},
   extraReducers: (builder) => {
     builder
